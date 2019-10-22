80ee307c9bb823937d3f487a24b9415e107011de
kvmanohar22 <kvmanohar22@gmail.com>
2019-10-22 11:00:43 +0530

    change dt

--- test/test_linear.cpp
+++ test/test_linear.cpp
@@ -14,7 +14,7 @@ int main() {
     auto t = linear_controller.t();
     auto x = linear_controller.step();
     cout << "t = " << t << "\t x = " << x.transpose() << '\n';
-    // std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
   }
 }
 
